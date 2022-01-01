03d8bd83b2825a91f777d06718a3cbbc599699e1
Rizart Dokollari <r.dokollari@gmail.com>
2022-01-01 03:10:32 +0000

    Remove stale statement

--- cicd-authorisation/lib/config.ts
+++ cicd-authorisation/lib/config.ts
@@ -3,7 +3,6 @@ import packageJson from "../../infrastructurerc.json";
 export class Config {
   public org = packageJson.org;
   public cicdEnvironment = "cicd";
-  public environments = ["test", "production", "dev"];
   public region = packageJson.region;
   public name = packageJson.name;
 }
