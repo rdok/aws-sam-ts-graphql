8d1e28aae586b55f9066afa1f39ab892ded8abdb
Rizart Dokollari <r.dokollari@gmail.com>
2022-01-02 04:12:01 +0000

    Assert authorisation for API Gateways tags & restapis

--- cicd-authorisation/__tests__/api-gateway.test.ts
+++ cicd-authorisation/__tests__/api-gateway.test.ts
@@ -1,6 +1,6 @@
 import { Stack } from "@aws-cdk/core";
 import { Config } from "../lib/config";
-import { Template } from "@aws-cdk/assertions";
+import { Match, Template } from "@aws-cdk/assertions";
 import { Role, User } from "@aws-cdk/aws-iam";
 import { ApiGatewayPolicy } from "../lib/api-gateway-policy";
 
@@ -15,7 +15,7 @@ const template = Template.fromStack(stack);
 test("Authorise API Gateway creation", () => {
   template.hasResourceProperties("AWS::IAM::ManagedPolicy", {
     PolicyDocument: {
-      Statement: [
+      Statement: Match.arrayWith([
         {
           Action: "apigateway:POST",
           Effect: "Allow",
@@ -26,8 +26,50 @@ test("Authorise API Gateway creation", () => {
             ],
           },
         },
-      ],
-      Version: "2012-10-17",
+      ]),
+    },
+  });
+});
+
+test("Authorise management of created API Gateway resources", () => {
+  template.hasResourceProperties("AWS::IAM::ManagedPolicy", {
+    PolicyDocument: {
+      Statement: Match.arrayWith([
+        {
+          Condition: {
+            StringLike: {
+              "aws:ResourceTag/aws:cloudformation:stack-name": stackRegex,
+            },
+          },
+          Action: "apigateway:*",
+          Effect: "Allow",
+          Resource: {
+            "Fn::Join": [
+              "",
+              ["arn:aws:apigateway:", { Ref: "AWS::Region" }, "::/restapis/*"],
+            ],
+          },
+        },
+      ]),
+    },
+  });
+});
+
+test("Authorise AWS SAM to tag API Gateway deployment & stage", () => {
+  template.hasResourceProperties("AWS::IAM::ManagedPolicy", {
+    PolicyDocument: {
+      Statement: Match.arrayWith([
+        {
+          Action: "apigateway:*",
+          Effect: "Allow",
+          Resource: {
+            "Fn::Join": [
+              "",
+              ["arn:aws:apigateway:", { Ref: "AWS::Region" }, "::/tags/*"],
+            ],
+          },
+        },
+      ]),
     },
   });
 });
--- cicd-authorisation/lib/api-gateway-policy.ts
+++ cicd-authorisation/lib/api-gateway-policy.ts
@@ -3,16 +3,36 @@ import { Effect, ManagedPolicy, PolicyStatement } from "@aws-cdk/aws-iam";
 import { RolePolicyProps } from "./types";
 
 export class ApiGatewayPolicy {
-  constructor(stack: Stack, { role }: RolePolicyProps) {
+  constructor(stack: Stack, { role, stackRegex }: RolePolicyProps) {
     const lambdaPolicy = new ManagedPolicy(stack, "ApiGatewayPolicy", {
       description: `Policy to manage API Gateway: ${stack.stackName}`,
       roles: [role],
     });
+
+    lambdaPolicy.addStatements(
+      new PolicyStatement({
+        effect: Effect.ALLOW,
+        resources: [`arn:aws:apigateway:${stack.region}::/tags/*`],
+        actions: ["apigateway:*"],
+      })
+    );
     lambdaPolicy.addStatements(
       new PolicyStatement({
         effect: Effect.ALLOW,
         resources: [`arn:aws:apigateway:${stack.region}::/restapis`],
         actions: ["apigateway:POST"],
+      })
+    );
+    lambdaPolicy.addStatements(
+      new PolicyStatement({
+        effect: Effect.ALLOW,
+        resources: [`arn:aws:apigateway:${stack.region}::/restapis/*`],
+        actions: ["apigateway:*"],
+        conditions: {
+          StringLike: {
+            "aws:ResourceTag/aws:cloudformation:stack-name": stackRegex,
+          },
+        },
       })
     );
   }
