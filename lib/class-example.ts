73e83b243edd3813f6facfe9322dead5cec1bec8
Rizart Dokollari <r.dokollari@gmail.com>
2022-01-02 18:48:46 +0000

    Remove stale files

--- lib/class-example.ts
+++
@@ -1,7 +0,0 @@
-export class ClassExample {
-  async functionExample(): Promise<string> {
-    const response = process.env.NODE_ENV;
-    if (response === undefined) throw Error("Environment NODE_ENV not set.");
-    return Promise.resolve(response);
-  }
-}
