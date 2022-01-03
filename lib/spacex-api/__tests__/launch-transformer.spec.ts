5465fac08539b346ce4c565a1681da9fd5c177cc
Rizart Dokollari <r.dokollari@gmail.com>
2022-01-03 13:54:07 +0000

    Refactor: cleanup

--- lib/spacex-api/__tests__/launch-transformer.spec.ts
+++ lib/spacex-api/__tests__/launch-transformer.spec.ts
@@ -2,37 +2,33 @@ import { company, datatype } from "faker";
 import { LaunchTransformer } from "../launch-transformer";
 
 it("transforms a launch", () => {
-  const { launchTransformer, launch } = makeLaunchTransformer();
+  const { launchTransformer, launch } = makeTransformer();
   const actual = launchTransformer.transform(launch);
-  expect(actual).toStrictEqual({
-    id: launch.id,
-    name: launch.name,
-  });
+  expect(actual).toStrictEqual({ id: launch.id, name: launch.name });
 });
 
 it("transforms a list of launches", () => {
-  const { launchTransformer, launches } =
-    makeLaunchTransformerWithMockedTransformFunc();
+  const { launchTransformer, launches } = makeMockedTransformer();
   launchTransformer.transformMany(launches);
   expect(launchTransformer.transform).toHaveBeenCalledTimes(launches.length);
 });
 
 it("returns the list of transformed launches", () => {
   const { launchTransformer, launches, mockedTransformValue } =
-    makeLaunchTransformerWithMockedTransformFunc();
+    makeMockedTransformer();
   const actual = launchTransformer.transformMany(launches);
   expect(actual).toEqual([mockedTransformValue, mockedTransformValue]);
 });
 
-function makeLaunchTransformer() {
+function makeTransformer() {
   const launchTransformer = new LaunchTransformer();
   const launch = makeLaunch();
   const launches = [launch, makeLaunch()];
   return { launchTransformer, launch, launches };
 }
 
-function makeLaunchTransformerWithMockedTransformFunc() {
-  const launchTransformerMaker = makeLaunchTransformer();
+function makeMockedTransformer() {
+  const launchTransformerMaker = makeTransformer();
   const mockedTransformValue = "mock-transform-value";
   launchTransformerMaker.launchTransformer.transform = jest
     .fn()
