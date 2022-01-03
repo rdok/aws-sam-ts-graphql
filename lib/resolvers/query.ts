aab3d3a6b1755f9104cdce761dd85165e5ee0caf
Rizart Dokollari <r.dokollari@gmail.com>
2022-01-03 15:47:58 +0000

    Add launches query

--- lib/resolvers/query.ts
+++ lib/resolvers/query.ts
@@ -1,12 +1,21 @@
 import { Context } from "../context";
+import { PaginationProps } from "../spacex-api/launch-api";
 
 const Query = {
-  launch: async (parent: unknown, args: { id: string }, context: Context) => {
+  healthCheck: () => "alive",
+  launch: async (parent: any, args: { id: string }, context: Context) => {
     const launchApi = context.dataSources.launchApi;
     const launch = await launchApi.find({ id: args.id });
     return context.ioc.launchTransformer.transform(launch);
   },
-  healthCheck: () => "alive",
+  async launches(parent: any, args: PaginationProps, context: Context) {
+    const launchApi = context.dataSources.launchApi;
+    const launches = await launchApi.query(args);
+    return {
+      ...args,
+      data: context.ioc.launchTransformer.transformMany(launches),
+    };
+  },
 };
 
 export { Query as default };
