880510dbf7bb3da8a26fe6b8d5cd73708e35ea18
Rizart Dokollari <r.dokollari@gmail.com>
2022-01-03 15:48:30 +0000

    Add launches to schema

--- lib/schema.ts
+++ lib/schema.ts
@@ -4,10 +4,26 @@ export const typeDefs = gql`
   type Query {
     healthCheck: String
     launch(id: ID!): Launch
+    launches(
+      """
+      The number of results to show.
+      """
+      limit: Int
+      """
+      The page number to render.
+      """
+      page: Int
+    ): Launches!
   }
 
   type Launch {
     id: ID!
     name: String!
+  }
+
+  type Launches {
+    limit: Int!
+    page: Int!
+    data: [Launch]!
   }
 `;
